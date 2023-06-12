759a56323cb17a93813e344d571f9dad94b6eeb6
jhoan david <rojasalmariojhoandvid@gmail.com>
2023-06-12 16:57:48 -0500

    form username

--- src/main/java/com/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/controllers/FormController.java
@@ -1,21 +1,26 @@
 package com.springboot.form.app.controllers;
 
+import com.springboot.form.app.models.domain.Usuario;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
+        model.addAttribute("titulo","Formulario Usuairo");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(Usuario usuario, Model model){
+        model.addAttribute("titulo", "Resultado Form");
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 }
